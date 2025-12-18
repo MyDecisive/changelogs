112ad6ef4e54e8b29f89d27ef8069840423ae139
xhsun <xhsun@users.noreply.github.com>
2025-12-18 17:59:30 +0000

    generate script

---
+++ scripts/generate.sh
@@ -0,0 +1,9 @@
+tracked=$(yq ".version" tracked.yaml)
+if [  "$1" = "`echo -e "$1\n$tracked" | sort -V | head -n1`" ]; then
+  echo "Already generated"
+else
+  export version=$1
+  composite --path $GITHUB_WORKSPACE/CHANGELOG.md --version $version
+  yq e -i '.version= env(version)' tracked.yaml
+  echo "GENERATED=1" >> "$GITHUB_OUTPUT"
+fi
