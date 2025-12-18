f043308e7a95d206feea9475b67726d78da1b436
xhsun <xhsun@users.noreply.github.com>
2025-12-18 18:19:19 +0000

    test

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -4,7 +4,7 @@ if [  "$1" = "`echo -e "$1\n$tracked" | sort -V | head -n1`" ]; then
   echo "Already generated"
 else
   export version=$1
-  composite --path $GITHUB_WORKSPACE/CHANGELOG.md --version $version
+  composite --config https://raw.githubusercontent.com/DecisiveAI/changelogs/refs/heads/ENG-972/scripts/composite/cliff.toml --path $GITHUB_WORKSPACE/CHANGELOG.md --version $version
   yq e -i '.version= env(version)' tracked.yaml
   echo "GENERATED=1" >> "$GITHUB_OUTPUT"
 fi
